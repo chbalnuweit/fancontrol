832abfcbd9fb4d2c23e5a0cfa585c91f94c2de93
chbalnuweit <chbalnuweit@gmail.com>
2021-11-08 15:26:14 +0100

    Drehzahlerkennung ohne Interrupt, InfluxDB Anbindung

--- fan_lib.py
+++ fan_lib.py
@@ -3,7 +3,7 @@ import os
 import threading
 import time
 import math
-
+import signal
 
 class Fan_Thread(threading.Thread):
     """
@@ -15,7 +15,6 @@ class Fan_Thread(threading.Thread):
     def __init__(self, pwm_pin, speed_pin):
         # Attribute initialisieren
         self.fanspeed = 0
-        self.rev_counter = 0
         self.pwm_pin = pwm_pin
         self.speed_pin = speed_pin
         
@@ -28,19 +27,48 @@ class Fan_Thread(threading.Thread):
         GPIO.setup(pwm_pin, GPIO.OUT)												# PWM Pin als Ausgang konfigurieren
         self.GPIOPWM = GPIO.PWM(self.pwm_pin, 25000) 	 							# PWM Pin konfigurieren mit 25 khz Grundfrequenz		
         
-        # Interrupt Pin fuer Drehzahlsignal konfigurieren
-        GPIO.setup(speed_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)					# Drehzahlpin mit Pullup konfigurieren
-        GPIO.add_event_detect(speed_pin, GPIO.RISING, callback=self.count_revs)		# ISR bei steigender Flanke am Drehzahlpin
-        
     def run(self):
         """
         Berechnen der Drehzahl im Sekundentakt im eigenen Thread. 
         """
-        while True:
-            self.rev_counter = 0
-            time.sleep(1)
-            self.fanspeed = math.floor(self.rev_counter / 2.0 * 60.0)
         
+        # Pin als Eingang mit internem Pullup gegen Vdd
+        GPIO.setup(self.speed_pin, GPIO.IN, pull_up_down = GPIO.PUD_UP)				
+        
+        # Initialisierung der internen Variablen
+        rev_counter = 0
+        ms_timer = 0
+        
+        # Initialen Zustand des Drehzahlsignals auslesen
+        state_prev = GPIO.input(self.speed_pin)
+        
+        while True:            
+            # 1 ms Timer Inkrement
+            time.sleep(0.001)
+            ms_timer += 1
+            
+            # aktuellen Zustand des Drehzahlsignals auslesen
+            state_now = GPIO.input(self.speed_pin)
+            
+            # bei steigender Flanke wird Counter inkrementiert
+            if state_now == GPIO.HIGH and state_prev == GPIO.LOW:
+                rev_counter += 1
+                
+            # aktuellen Zustand zwischenspeichern
+            state_prev = state_now
+            
+            # Nach einer Sekunde die Drehzahl aus Counter berechnen
+            if ms_timer >= 1000:
+                rpm = rev_counter*60.0/2.0
+                
+                # Variablen zuruecksetzen
+                rev_counter = 0
+                ms_timer = 0
+                
+                # Drehzahl auf Object-Variable schreiben fuer Getter-Funktion
+                self.fanspeed = rpm
+   
+    
     def count_revs(self, speed_pin):
         """
         Interrupt Service Routine fuer Drehzahlsignal erkennung.
@@ -48,7 +76,8 @@ class Fan_Thread(threading.Thread):
         Parameter:
         speed_pin -- GPIO mit dem ausloesenden add_event_detect
         """
-        self.rev_counter += 1
+        global rev_counter
+        rev_counter += 1
 
     def get_fanspeed(self):
         """ Getter-Funktion fuer Luefter Drehzahl in RPM """
